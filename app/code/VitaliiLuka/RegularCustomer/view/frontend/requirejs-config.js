a8d294aab5570e55417b8cf2f9ee6819dc104de8
Luka <luka@luka-ubuntu-20.04>
2021-04-04 22:02:25 +0300

    #15 Knockout.js, UI Components: Rewritten button to Knockout component. Hide button if the product has already been requested.

--- app/code/VitaliiLuka/RegularCustomer/view/frontend/requirejs-config.js
+++ app/code/VitaliiLuka/RegularCustomer/view/frontend/requirejs-config.js
@@ -1,7 +1,7 @@
 var config = {
     map: {
         '*': {
-            vitaliiLukaRegularCustomerButton: 'VitaliiLuka_RegularCustomer/js/button',
+            vitaliiLukaRegularCustomerButton: 'VitaliiLuka_RegularCustomer/js/view/button',
             vitaliiLukaRegularCustomerForm: 'VitaliiLuka_RegularCustomer/js/view/form',
             vitaliiLukaRegularCustomerSubmitForm: 'VitaliiLuka_RegularCustomer/js/action/submit-form'
         }
